685e0c5b6407f6c5e7940c6d728f7aeeb80de872
Isabelle Bourgeois <easybeau@gmail.com>
2021-11-23 06:08:04 +0100

    Add files via upload

---
+++ assets/javascripts/lunr/lunr.store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Nouvelle friperie !",
+        "excerpt":"Voilà le projet d’une friperie moderne est en cours. Vous pourrez y trouver des vêtements et accessoires de seconde main issus des dernières tendances. ","categories": ["nouvelle","friperie"],
+        "tags": [],
+        "url": "/nouvelle/friperie/2021/11/23/nouvelle-friperie.html"
+      }]
