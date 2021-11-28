b238e7528a8eecc36f343f8276893516dacd3793
Isabelle Bourgeois <easybeau@gmail.com>
2021-11-28 07:34:52 +0100

    Delete assets directory

--- assets/javascripts/lunr/lunr.store.js
+++
@@ -1,6 +0,0 @@
-var store = [{
-        "title": "Nouvelle friperie !",
-        "excerpt":"Voilà, le projet d’une nouvelle friperie moderne dans le Canton de Vaud est en cours. Vous pourrez y trouver des vêtements et accessoires de seconde main issus des dernières tendances. ","categories": ["friperie"],
-        "tags": [],
-        "url": "/friperie/2021/11/23/nouvelle-friperie.html"
-      }]
