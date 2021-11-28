ca425415d63aae3cc03c90f5a35fac1b359550fa
Isabelle Bourgeois <easybeau@gmail.com>
2021-11-28 07:37:27 +0100

    Add files via upload

---
+++ assets/javascripts/lunr/lunr.store.js
@@ -0,0 +1,11 @@
+var store = [{
+        "title": "Nouvelle friperie !",
+        "excerpt":"Voilà, le projet d’une nouvelle friperie moderne dans le Canton de Vaud est en cours. Vous pourrez y trouver des vêtements et accessoires de seconde main issus des dernières tendances. ","categories": ["friperie"],
+        "tags": [],
+        "url": "/friperie/2021/11/23/nouvelle-friperie.html"
+      },{
+        "title": "Comment donner ses vêtements",
+        "excerpt":"Deux options s’offrent à vous pour donner des vêtements :       Déposer vos sacs chez l’un des commerçants partenaires.         Organiser un ramassage à la maison   ","categories": ["donner"],
+        "tags": [],
+        "url": "/donner/2021/11/27/comment-donner-des-v%C3%AAtements.html"
+      }]
